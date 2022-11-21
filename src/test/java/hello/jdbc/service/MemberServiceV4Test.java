175be07c4082598d777ba931cf4b236d808b79eb
yesol <ysoli36512@gmail.com>
2022-11-21 11:04:58 +0900

    스프링 예외 추상화 적용

--- src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -1,10 +1,10 @@
 package hello.jdbc.service;
-
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
-import hello.jdbc.repository.MemberRepositoryV3;
 import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV4_2;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,15 +13,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
 import javax.sql.DataSource;
 import java.sql.SQLException;
-
+import static hello.jdbc.connection.ConnectionConst.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 /**
- * 트랜잭션 - DataSource, transactionManager 자동 등록
+ * 예외 누수 문제 해결
+ * SQLException 제거
+ *
+ * MemberRepository 인터페이스 의존
  */
 @Slf4j
 @SpringBootTest
@@ -29,53 +33,38 @@ class MemberServiceV4Test {
     public static final String MEMBER_A = "memberA";
     public static final String MEMBER_B = "memberB";
     public static final String MEMBER_EX = "ex";
-
     @Autowired
-    private MemberRepository memberRepository;
+    MemberRepository memberRepository;
     @Autowired
-    private MemberServiceV4 memberService;
-
+    MemberServiceV4 memberService;
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
     @TestConfiguration
-    static class TestConfig{
-
+    static class TestConfig {
         private final DataSource dataSource;
-
-        public TestConfig(DataSource dataSource){
+        public TestConfig(DataSource dataSource) {
             this.dataSource = dataSource;
         }
-
         @Bean
         MemberRepository memberRepository() {
-
-            return new MemberRepositoryV4_1(dataSource);
+            //return new MemberRepositoryV4_1(dataSource); //단순 예외 변환
+            return new MemberRepositoryV4_2(dataSource); //단순 예외 변환
         }
         @Bean
         MemberServiceV4 memberServiceV4() {
-
             return new MemberServiceV4(memberRepository());
         }
     }
-
-    /*  @BeforeEach
-    void before() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-        memberRepository = new MemberRepositoryV3(dataSource);
-        memberService = new MemberServiceV3_3(memberRepository);
-    }*/
-
     @Test
     void AopCheck() {
         log.info("memberService class={}", memberService.getClass());
         log.info("memberRepository class={}", memberRepository.getClass());
-        assertThat(AopUtils.isAopProxy(memberService)).isTrue();
-        assertThat(AopUtils.isAopProxy(memberRepository)).isFalse();
-    }
-
-    @AfterEach
-    void after()  {
-        memberRepository.delete(MEMBER_A);
-        memberRepository.delete(MEMBER_B);
-        memberRepository.delete(MEMBER_EX);
+        Assertions.assertThat(AopUtils.isAopProxy(memberService)).isTrue();
+        Assertions.assertThat(AopUtils.isAopProxy(memberRepository)).isFalse();
     }
     @Test
     @DisplayName("정상 이체")
@@ -96,7 +85,7 @@ class MemberServiceV4Test {
     }
     @Test
     @DisplayName("이체중 예외 발생")
-    void accountTransferEx() {
+    void accountTransferEx() throws SQLException {
         //given
         Member memberA = new Member(MEMBER_A, 10000);
         Member memberEx = new Member(MEMBER_EX, 10000);
